e25d6aafa8a4e251f5e92b27e553ad6103a20ea4
dermatologist <github@gulfdoctor.net>
2018-01-22 10:07:47 -0500

    Dispatch Added.

--- app/js/components/App.jsx
+++ app/js/components/App.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 
 import {Link} from 'react-router'
+import {connect} from "react-redux";
+import {loadManifest} from "../actions/contextActions";
 
+@connect((store) => {
+    return {
+        href: store.context.manifest.activities.openmrs.href,
+    };
+})
 export default class App extends React.Component {
+    componentWillMount() {
+        this.props.dispatch(loadManifest());
+    }
+
     render() {
         return (
-                <div>
-                    <h1>React Example</h1>
-                    <ul role="nav">
-                        <li><Link to="/findPatient" activeClassName="active">Find</Link></li>
-                        <li><Link to="/help" activeClassName="active">Help</Link></li>
-                    </ul>
-                    {this.props.children}
-                </div>
+            <div>
+                <h1>React Example</h1>
+                <ul role="nav">
+                    <li><Link to="/findPatient" activeClassName="active">Find</Link></li>
+                    <li><Link to="/help" activeClassName="active">Help</Link></li>
+                </ul>
+                {this.props.children}
+                {this.props.href}
+            </div>
         )
     }
 }
