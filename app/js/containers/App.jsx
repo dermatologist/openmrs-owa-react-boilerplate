b9fdec52f8970e7897734519d09c460d872347e1
dermatologist <github@gulfdoctor.net>
2018-01-23 19:41:54 -0500

    Panels added.

--- app/js/containers/App.jsx
+++ app/js/containers/App.jsx
@@ -3,6 +3,14 @@ import React from 'react';
 import {Link} from 'react-router'
 import {connect} from "react-redux";
 import {loadManifest} from "../actions/contextActions";
+import FindPatient from "../components/FindPatient";
+import Help from "../components/Help";
+
+import RGL, { WidthProvider } from "react-grid-layout";
+import '../../../node_modules/react-grid-layout/css/styles.css';
+import '../../../node_modules/react-resizable/css/styles.css';
+
+const ReactGridLayout = WidthProvider(RGL);
 
 @connect((store) => {
     return {
@@ -27,12 +35,22 @@ export default class App extends React.Component {
                     <li><Link to="/findPatient" activeClassName="active">Find</Link></li>
                     <li><Link to="/help" activeClassName="active">Help</Link></li>
                 </ul>
-                {this.props.children}
                 <h2>{this.props.name} | Version: {this.props.version}</h2>
                 <h3>Developer: {this.props.developer} | website: {this.props.website}</h3>
                 <h4>Description: {this.props.description}</h4>
                 <h5>OpenMRS root: {this.props.href}</h5>
 
+                <ReactGridLayout className="layout" cols={12} rowHeight={30} width={1200}>
+                    <div key="a" data-grid={{x: 0, y: 0, w: 1, h: 2, static: true}}>
+                        <FindPatient/>
+                    </div>
+                    <div key="b" data-grid={{x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4}}>
+                        {this.props.children}
+                    </div>
+                    <div key="c" data-grid={{x: 4, y: 0, w: 1, h: 2}}>
+                        <Help/>
+                    </div>
+                </ReactGridLayout>
             </div>
         )
     }
