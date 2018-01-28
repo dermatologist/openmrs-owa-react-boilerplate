a2498af1ed83d7ee525699f0008e535719a10694
dermatologist <github@gulfdoctor.net>
2018-01-27 20:33:13 -0500

    Test running.

--- app/js/containers/App.spec.js
+++ app/js/containers/App.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Enzyme, {mount} from 'enzyme'
+import Enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import {App} from './App'
 
@@ -10,7 +10,7 @@ function setup() {
         loadManifest: jest.fn(),
     };
 
-    const enzymeWrapper = mount(<App {...props} />);
+    const enzymeWrapper = shallow(<App {...props} />);
 
     return {
         props,
@@ -20,25 +20,10 @@ function setup() {
 
 describe('components', () => {
     describe('App', () => {
-        it('should render self and subcomponents', () => {
+        it('should render self', () => {
             const {enzymeWrapper} = setup();
 
-            expect(enzymeWrapper.find('App').hasClass('App')).toBe(true)
-
-            // expect(enzymeWrapper.find('h1').text()).toBe('todos')
-            //
-            // const todoInputProps = enzymeWrapper.find('TodoTextInput').props()
-            // expect(todoInputProps.newTodo).toBe(true)
-            // expect(todoInputProps.placeholder).toEqual('What needs to be done?')
+            expect(enzymeWrapper.find('h1').text()).toBe('React Example')
         });
-
-        it('should call addTodo if length of text is greater than 0', () => {
-            const {enzymeWrapper, props} = setup()
-            // const input = enzymeWrapper.find('TodoTextInput')
-            // input.props().onSave('')
-            // expect(props.addTodo.mock.calls.length).toBe(0)
-            // input.props().onSave('Use Redux')
-            // expect(props.addTodo.mock.calls.length).toBe(1)
-        })
     })
 });
